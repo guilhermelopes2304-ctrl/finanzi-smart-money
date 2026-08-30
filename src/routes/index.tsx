88385010b40e1ffb4c747ff92345346c6d0663a7
guilhermelopes2304-ctrl <guilhermelopes2304@gmail.com>
2026-08-30 01:06:04 -0300

    chore(landing): isolate generated editorial layout from legacy prettier rule

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 import { createFileRoute, Link } from "@tanstack/react-router";
 import { ArrowRight, Check, ChevronDown, Mic, ShieldCheck } from "lucide-react";
 import { useEffect, useRef, useState } from "react";
